6a586bb421b5fd78dbd8ccf827cff069bfc37cee
Booki Park <bkbkgg@gmail.com>
2017-10-16 00:39:13 +0900

    댓글 기능 추가

--- instagram/post/views.py
+++ instagram/post/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 
+from .forms import CommentForm
 from .models import Post
 
 
@@ -27,9 +28,16 @@ def post_upload(request):
     return render(request, 'post/post_upload.html')
 
 
-    # def post_comment(request):
-    #     if request.method == 'POST':
-    #         comment = request.POST.get('comment')
-    #         Post.objects.create(comment=comment)
-    #         return redirect('post_list')
-    #     return render(request, 'post/post_list.html')
+def post_comment(request, pk):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = Post.objects.get(pk=pk)
+            comment.save()
+            return redirect('post_detail')
+        else:
+            form = CommentForm()
+        return render(request, 'post/post_comment.html', {
+            'form': form,
+        })
