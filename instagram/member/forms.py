8053b89038769a0535ec9f3463af3f83a6225594
Booki Park <bkbkgg@gmail.com>
2017-10-17 15:43:37 +0900

    form에서 로그인 동작을 모두 처리하도록 수정

--- instagram/member/forms.py
+++ instagram/member/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login, authenticate
 
 User = get_user_model()
 
@@ -28,18 +28,46 @@ class SignupForm(forms.Form):
         return data
 
 
-class LoginForm(forms.Form):
+class SigninForm(forms.Form):
     username = forms.CharField(
         widget=forms.TextInput(
             attrs={
-                'class': 'form-control'
+                'class': 'form-control',
             }
         )
     )
     password = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                'class': 'form-control'
+                'class': 'form-control',
             }
         )
     )
+
+    # 초기화 함수 __init__, self.user 사용 위해 정의
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user = None
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+        self.user = authenticate(
+            username=username,
+            password=password,
+        )
+        if not self.user:
+            raise forms.ValidationError('Login failed')
+        else:
+            # 동적으로 작동
+            setattr(self, 'signin', self._signin)
+
+    def _signin(self, request):
+        """
+
+        :param request: django.auth.login()에 주어질 HttpRequest 객체
+        :return:
+        """
+        if self.user:
+            login(request, self.user)
--- instagram/member/views.py
+++ instagram/member/views.py
@@ -1,8 +1,8 @@
-from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth import logout, get_user_model
 # from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
-from member.forms import SignupForm, LoginForm
+from member.forms import SignupForm, SigninForm
 
 # User model을 가져올 때는 이 함수를 써서 import 하는 것을 추천
 User = get_user_model()
@@ -29,23 +29,21 @@ def signup(request):
 
 
 def signin(request):
+    """
+    is_valid()에서 주어진 username/password를 사용한 authenticate 실행
+    성공시 login(request) method를 사용할 수 있음
+    :param request:
+    :return:
+    """
     if request.method == 'POST':
-        form = LoginForm(request.POST)
+        form = SigninForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('post_list')
+            form.signin(request)
+            return redirect('post_list')
     else:
-        form = LoginForm()
-        context = {
-            'form': form,
-        }
-        return render(request, 'member/login.html', context)
+        return render(request, 'member/login.html')
 
 
 def signout(request):
     logout(request)
-    return redirect('signin')
+    return redirect('post_list')
