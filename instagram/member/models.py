c905b6654fe4713ca7d576235c843883784ed560
Booki Park <bkbkgg@gmail.com>
2017-10-19 14:11:56 +0900

    사진 업로드 버튼 이동

--- instagram/member/models.py
+++ instagram/member/models.py
@@ -1,5 +1,20 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager as DjangoUserManager
+from django.db import models
+
+
+class UserManager(DjangoUserManager):
+    def create_superuser(self, *args, **kwargs):
+        return super().create_superuser(age=30, *args, **kwargs)
 
 
 class User(AbstractUser):
-    pass
+    # FileField는 문자열 필드라 null=True가 들어가지 않는다
+    img_profile = models.ImageField(
+        upload_to='user',
+        blank=True,
+    )
+    age = models.IntegerField()
+
+    objects = UserManager()
+
+    # REQUIRED_FIELDS = AbstractUser.REQUIRED_FIELDS + ['age']
