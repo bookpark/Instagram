082886fd2d90bc4322384dc92f092870083d3355
Booki Park <bkbkgg@gmail.com>
2017-10-31 16:33:27 +0900

    nginx conf, aws 설정 변경

--- instagram/config/settings.py
+++ instagram/config/settings.py
@@ -55,6 +55,8 @@ FACEBOOK_SCOPE = [
 AWS_ACCESS_KEY_ID = config_secret_common['aws']['access_key_id']
 AWS_SECRET_ACCESS_KEY = config_secret_common['aws']['secret_access_key']
 AWS_STORAGE_BUCKET_NAME = config_secret_common['aws']['s3_bucket_name']
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = 'ap-northeast-2'
 
 # AWS Storage
 STATICFILES_LOCATION = 'static'
