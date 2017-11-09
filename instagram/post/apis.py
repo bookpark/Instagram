872aa287c3b2053053d82dbdc9117d88db798c91
Booki Park <bkbkgg@gmail.com>
2017-11-09 14:32:29 +0900

    generics api view로 refactor

--- instagram/post/apis.py
+++ instagram/post/apis.py
@@ -1,39 +1,16 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import Post
 from .serializers import PostSerializer
 
 
-class PostList(APIView):
-    def get(self, request):
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (
+        IsAuthenticatedOrReadOnly,
+    )
 
-    def post(self, request):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(author=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class PostDetail(APIView):
-    def get_object(self, post_pk):
-        try:
-            return Post.objects.get(pk=post_pk)
-        except Post.DoesNotExist:
-            raise Http404
-
-    def get(self, request, post_pk):
-        post = self.get_object(post_pk)
-        seriazlier = PostSerializer(post)
-        return Response(seriazlier.data)
-
-    def delete(self, request, post_pk):
-        post = self.get_object(post_pk)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
