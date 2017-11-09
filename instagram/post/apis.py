ab60be2370c4c52805f6c5df8de94e2f7d2f1f10
Booki Park <bkbkgg@gmail.com>
2017-11-09 12:31:00 +0900

    PostDetail 추가

--- instagram/post/apis.py
+++ instagram/post/apis.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -10,3 +12,28 @@ class PostList(APIView):
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(author=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostDetail(APIView):
+    def get_object(self, post_pk):
+        try:
+            return Post.objects.get(pk=post_pk)
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request, post_pk):
+        post = self.get_object(post_pk)
+        seriazlier = PostSerializer(post)
+        return Response(seriazlier.data)
+
+    def delete(self, request, post_pk):
+        post = self.get_object(post_pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
