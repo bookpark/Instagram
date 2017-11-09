f04c41268310f23dd91478305ebd7a3803208d92
Booki Park <bkbkgg@gmail.com>
2017-11-09 12:28:27 +0900

    refactored urls

--- instagram/post/urls.py
+++ instagram/post/urls.py
@@ -20,8 +20,8 @@ urlpatterns = [
     url(r'^', views.post_list, name='post_list'),
     url(r'^(?P<post_pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^upload/$', views.post_upload, name='post_upload'),
-    url(r'^(?P<post_pk>\d+)/comments/add/', views.post_comment, name='post_comment'),
     url(r'^(?P<post_pk>\d+)/delete$', views.post_delete, name='post_delete'),
     url(r'^(?P<post_pk>\d+)/like-toggle$', views.post_like_toggle, name='post_like_toggle'),
+    url(r'^(?P<post_pk>\d+)/comments/add/', views.post_comment, name='post_comment'),
     url(r'^(?P<comment_pk>\d+)/comments/delete/', views.comment_delete, name='comment_delete'),
 ]
