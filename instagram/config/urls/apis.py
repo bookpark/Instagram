0718c27641a6eef7043a8832701551372eaa6b09
Booki Park <bkbkgg@gmail.com>
2017-11-15 02:14:08 +0900

    SendSMSAPIView 구현

--- instagram/config/urls/apis.py
+++ instagram/config/urls/apis.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url, include
 
+from utils.sms.apis import SendSMS
+
 urlpatterns = [
     url(r'^members/', include('member.urls.apis', namespace='member')),
     url(r'^posts/', include('post.urls.apis', namespace='post')),
+    url(r'^utils/sms/send/$', SendSMS.as_view(), name='send-sms'),
 ]
